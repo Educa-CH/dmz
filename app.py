653ff416a7e1fbd29781e601313d464adf477dd9
Davide Ravasi <davideravasi01@gmail.com>
2025-07-30 13:37:03 +0200

    Implementation of the Procivis API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
@@ -6,6 +7,7 @@ from io import BytesIO
 import csv
 import os
 import requests
+import http.client
 import base64
 import qrcode
 
@@ -68,12 +70,84 @@ def upload_file():
 
             data = parse_flat_csv(filepath)
 
-
-            # API Call (Platzhalter-URL)
-            api_url = 'https://api.example.com/endpoint'  # ← später ersetzen
             try:
                 
+                # printing .csv data
                 print(data)
+
+
+                # Step 1: Retrieving all the credential schemas
+                # In a real world scenario you would filter the schema you need by name
+                accessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJXcGtGQ3Zhbjltbk13U29lTmdITHlKdDBGZmZpRmlEY1J2eFJvdFFNWFFnIn0.eyJleHAiOjE3NTM5MDU3NzEsImlhdCI6MTc1Mzg2MjU3MSwianRpIjoidHJydGNjOmExMzVlZGNhLTU5NDEtNDA2MC05NGU0LTJhMDdmZTNhODViNyIsImlzcyI6Imh0dHBzOi8va2V5Y2xvYWsudHJpYWwucHJvY2l2aXMtb25lLmNvbS9yZWFsbXMvdHJpYWwiLCJhdWQiOlsib25lLWJmZi1jbGllbnQiLCJhY2NvdW50Il0sInN1YiI6IjI2YmZjZDBiLTRlNWQtNDQ5YS1hNWQ3LWMzYWVkZDljNWE3NSIsInR5cCI6IkJlYXJlciIsImF6cCI6Im9uZS1lZHVjYSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiLyoiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiRWR1Y2FfRURJVE9SIiwidW1hX2F1dGhvcml6YXRpb24iLCJkZWZhdWx0LXJvbGVzLXRyaWFsIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJncm91cCBwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjbGllbnRIb3N0IjoiMTAuMS4yLjEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtb25lLWVkdWNhIiwiY2xpZW50QWRkcmVzcyI6IjEwLjEuMi4xIiwiY2xpZW50X2lkIjoib25lLWVkdWNhIn0.ZNyu3CqlkKGB1v40PPRJiAlenrlVuAcWeAeu7NeF7PxSBIUt2DY2qegTly1IWq5_XL3U_j3b6mKa-Th1BOtMQ5k4KzLx_ffJ3LAMHiQDRfjD6Pemdh6b9O93MKPQ26Jo1noOy5cI0F7DXcHRR0EptOhYneGYDNaoE9yVsS9FO3yT1UGqtkzrashos1H-rjrSzLUK1IM8xpFPX-9fAn294azaWk66nhF7xLlvIqlvYK_yhreBmHgrvVcrYEEWnvAMZ4e001RVeDAZd92dm4STNAPM0peFzR0-2QRdKW_l_MWD5bqvL1q3RB5n-VZo8yDp1aGBrTc8aRmgZtG45V7yaA'
+                connection = http.client.HTTPSConnection(host='api.trial.procivis-one.com')
+                
+                baseHeaders = {
+                'Accept': 'application/json',
+                'Authorization': f'Bearer {accessToken}' 
+                }
+                connection.request("GET", "/api/credential-schema/v1", '', baseHeaders)
+                schemeListResponse = connection.getresponse()
+
+                # Getting all the schemas
+                schemeList = schemeListResponse.read().decode('utf-8')
+                #print(json.dumps(schemeListString, indent=2))
+
+                #TODO: Selecting the schema you want from the scheme list
+
+                # Step 2
+                # Retrieving the schema we need
+
+                #hardcoded in this example, from the json above
+                schemaNeeded = '66b0d9ea-6326-4539-85f0-c177a7820abe'
+                schemaEndpoint = f"/api/credential-schema/v1/{schemaNeeded}"
+                connection.request("GET", schemaEndpoint, '', baseHeaders)
+                chosenSchemaResponse = connection.getresponse()
+                chosenSchema = chosenSchemaResponse.read().decode('utf-8')
+                #print(json.dumps(correctSchemeString, indent=2))
+
+                # Step 3
+                # Retrieve the right identifier to be used as issue > https://docs.procivis.ch/reference/desk/list-identifiers
+                connection.request("GET", '/api/identifier/v1', '', baseHeaders)
+                allIdentifiersResponse = connection.getresponse()
+                allIdentifiers = allIdentifiersResponse.read().decode('utf-8')
+
+                # Step 4
+                # Ceate the credential by using the ID of the claim in the schema
+                # it will return the credential ID
+                #TODO: Using the right schema ID, obtained in step 2
+                #TODO: Using the right issuer ID, obtained in step 3
+                credentialPayload = json.dumps({
+                "credentialSchemaId": "66b0d9ea-6326-4539-85f0-c177a7820abe",
+                "issuer": "f2164b6f-db9c-4807-94a8-9aad37c0bd4b",
+                "protocol": "OPENID4VCI_DRAFT13_SWIYU",
+                "claimValues": [
+                    {
+                    "claimId": "6b683bf6-1fa6-42fe-8ea5-30e2527eb0af",
+                    "path": "Field",
+                    "value": "Davide",
+                    }
+                ]
+                })
+                credentialHeaders = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': f'Bearer {accessToken}' 
+                }
+                connection.request("POST", "/api/credential/v1", credentialPayload, credentialHeaders)
+                credentialCreatedResponse = connection.getresponse()
+                credentialCreated = credentialCreatedResponse.read().decode("utf-8")
+
+                # Step 5
+                # Issuing the credential
+                #TODO: using the ID of the credential created in step 4
+                #hardcoded at the moment - HackatonTest schema
+                credentialId = '2714aa43-4b54-4cde-b064-4d0e4a7424b3'
+                issuingCredentialEndpoint = f"/api/credential/v1/{credentialId}/share"
+                connection.request("POST", issuingCredentialEndpoint, '', baseHeaders)
+                credentialIssuedResponse = connection.getresponse()
+                credentialIssued = credentialIssuedResponse.read().decode("utf-8")
+                
+
 
                 #response = requests.post(api_url, json=data)
                 #return jsonify({
@@ -157,4 +231,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-    
