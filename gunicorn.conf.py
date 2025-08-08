607653cf7fe3c869041c68deedb503d7139c4ece
Kevin Saner <kevin.saner91@hotmail.com>
2025-08-08 10:20:41 +0200

    added server config

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+bind = "dmz.educa.ch:443"
+certfile = "/usr/local/share/certs/educa.ch/fullchain.pem"
+keyfile = "/usr/local/share/certs/educa.ch/privkey.pem"
+workers = 2  # Number of worker processes to spawn
+threads = 4  # Number of threads per worker
+worker_class = "gthread"  # Worker class to use
+accesslog = 'logs/access'
+errorlog = 'logs/error'
+capture_output = True
+timeout = 120
+
+#gunicorn app:app -c gunicorn.conf.py --log-level=debug
